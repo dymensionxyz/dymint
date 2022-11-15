7b8e916ee908d615f4a9044b7525ac0584adfc73
omritoptix <omritoptix@gmail.com>
2022-11-15 14:39:36 +0200

    Added payload proto and changed the proto script to generate proto files in a relative directory.

--- proto/protoc.sh
+++ proto/protoc.sh
@@ -2,5 +2,9 @@
 
 set -eo pipefail
 
-buf generate --path="./proto/dalc" --template="buf.gen.yaml" --config="buf.yaml"
-buf generate --path="./proto/dymint" --template="buf.gen.yaml" --config="buf.yaml"
+# Generate the `types` proto files
+buf generate --path="./proto/types/dalc" --template="buf.gen.yaml" --config="buf.yaml"
+buf generate --path="./proto/types/dymint" --template="buf.gen.yaml" --config="buf.yaml"
+
+# Generate the `test` proto files
+buf generate --path="./proto/test" --template="buf.gen.yaml" --config="buf.yaml"
