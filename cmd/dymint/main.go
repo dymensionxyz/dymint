d808ecb662fd70eb60e301f50644b0f479c8d98f
Omri <omritoptix@gmail.com>
2023-01-10 15:42:50 +0100

    Added show-sequencer command to print the sequencer pubkey. (#178)

--- cmd/dymint/main.go
+++ cmd/dymint/main.go
@@ -14,6 +14,7 @@ func main() {
 	rootCmd := commands.RootCmd
 	rootCmd.AddCommand(
 		commands.InitFilesCmd,
+		commands.ShowSequencer,
 		debug.DebugCmd,
 		cli.NewCompletionCmd(rootCmd, true),
 	)
