2a319770935c883342f21261ff1e825c52445dec
omritoptix <omritoptix@gmail.com>
2023-05-21 19:23:55 +0200

    Added status middleware to track node health and generate proper response.

---
+++ rpc/middleware/status.go
@@ -0,0 +1,48 @@
+package middleware
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/dymensionxyz/dymint/rpc/sharedtypes"
+	"github.com/tendermint/tendermint/libs/log"
+)
+
+// StatusMiddleware is a struct that holds the health status of the node.
+type StatusMiddleware struct {
+	healthStatus *sharedtypes.HealthStatus
+}
+
+// NewStatusMiddleware creates and returns a new Status instance with the given healthStatus.
+func NewStatusMiddleware(healthStatus *sharedtypes.HealthStatus) *StatusMiddleware {
+	return &StatusMiddleware{
+		healthStatus: healthStatus,
+	}
+}
+
+// Handler returns a MiddlewareFunc that checks the node's health status.
+func (s *StatusMiddleware) Handler(logger log.Logger) HandlerFunc {
+	return func(h http.Handler) http.Handler {
+		return status(s.healthStatus, h, logger)
+	}
+}
+
+// status is a middleware that checks if the node is healthy.
+// If the node is not healthy, it returns a 503 Service Unavailable.
+func status(healthStatus *sharedtypes.HealthStatus, h http.Handler, logger log.Logger) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		isHealthy, err := healthStatus.Get()
+		if !isHealthy {
+			w.WriteHeader(http.StatusServiceUnavailable)
+			if err == nil {
+				err = errors.New("node is unhealthy")
+			}
+			_, err := w.Write([]byte(err.Error()))
+			if err != nil {
+				logger.Error("failed to write response", "error", err)
+			}
+			return
+		}
+		h.ServeHTTP(w, r)
+	})
+}
