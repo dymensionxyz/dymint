2ddda161b5dcf9b9611f122f841ea3c0eeaa10c8
Michael Tsitrin <michael@dymension.xyz>
2023-05-21 12:06:05 +0300

    cleanup

--- config/defaults.go
+++ config/defaults.go
@@ -2,8 +2,6 @@ package config
 
 import (
 	"time"
-
-	"github.com/dymensionxyz/dymint/settlement"
 )
 
 const (
@@ -15,25 +13,17 @@ const (
 
 // DefaultNodeConfig keeps default values of NodeConfig
 var DefaultNodeConfig = NodeConfig{
-	RootDir:            "",
-	DBPath:             "",
-	P2P:                P2PConfig{ListenAddress: DefaultListenAddress, Seeds: ""},
-	RPC:                RPCConfig{},
-	Aggregator:         true,
-	BlockManagerConfig: BlockManagerConfig{BlockTime: 200 * time.Millisecond, NamespaceID: "000000000000ffff", BatchSyncInterval: time.Second * 30, DABlockTime: 30 * time.Second, BlockBatchSize: 500, BlockBatchSizeBytes: 1500000},
-	DALayer:            "mock",
-	DAConfig:           "",
-	SettlementLayer:    "mock",
-	SettlementConfig: settlement.Config{
-		KeyringBackend: "test",
-		NodeAddress:    "",
-		KeyRingHomeDir: "",
-		DymAccountName: "",
-		RollappID:      "",
-		GasLimit:       0,
-		GasPrices:      "",
-		GasFees:        "",
-	},
+	P2P: P2PConfig{
+		ListenAddress: DefaultListenAddress,
+		Seeds:         ""},
+	Aggregator: true,
+	BlockManagerConfig: BlockManagerConfig{
+		BlockTime:           200 * time.Millisecond,
+		NamespaceID:         "000000000000ffff",
+		BatchSyncInterval:   time.Second * 30,
+		DABlockTime:         30 * time.Second,
+		BlockBatchSize:      500,
+		BlockBatchSizeBytes: 1500000},
+	DALayer:         "mock",
+	SettlementLayer: "mock",
 }
-
-//FIXME: default config with mocks should be enoguh to run
