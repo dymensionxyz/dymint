14f077ad348d0082e1e7fdb71c37131fd9ff87a9
Michael Tsitrin <michael@dymension.xyz>
2023-09-27 15:11:11 +0300

    added missing return statement. added clientID to the subscription log

--- utils/events.go
+++ utils/events.go
@@ -22,7 +22,8 @@ func SubscribeAndHandleEvents(ctx context.Context, pubsubServer *pubsub.Server, 
 		case event := <-subscription.Out():
 			callback(event)
 		case <-subscription.Cancelled():
-			logger.Info("Subscription canceled")
+			logger.Info(clientID + " subscription canceled")
+			return
 		}
 	}
 }
