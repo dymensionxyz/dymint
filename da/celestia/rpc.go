cb2b9f39e79315ce9b85b6523d79ac33538a005d
Sergi Rene <sergi@datahop.network>
2024-01-29 21:03:08 +0100

    added extra rpc calls for celestia da

--- da/celestia/rpc.go
+++ da/celestia/rpc.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/rollkit/celestia-openrpc/types/blob"
+	"github.com/rollkit/celestia-openrpc/types/header"
 	"github.com/rollkit/celestia-openrpc/types/share"
 	"github.com/rollkit/celestia-openrpc/types/state"
 
@@ -39,4 +40,29 @@ func (c *OpenRPC) SubmitPayForBlob(
 // GetAll gets all blobs.
 func (c *OpenRPC) GetAll(ctx context.Context, height uint64, namespaces []share.Namespace) ([]*blob.Blob, error) {
 	return c.rpc.Blob.GetAll(ctx, height, namespaces)
+}
+
+// Submit  blobs.
+func (c *OpenRPC) Submit(ctx context.Context, blobs []*blob.Blob, options *openrpc.SubmitOptions) (uint64, error) {
+	return c.rpc.Blob.Submit(ctx, blobs, options)
+}
+
+// Getting proof for submitted blob
+func (c *OpenRPC) GetProof(ctx context.Context, height uint64, namespace share.Namespace, commitment blob.Commitment) (*blob.Proof, error) {
+	return c.rpc.Blob.GetProof(ctx, height, namespace, commitment)
+}
+
+// Get blob for a specific share commitment
+func (c *OpenRPC) Get(ctx context.Context, height uint64, namespace share.Namespace, commitment blob.Commitment) (*blob.Blob, error) {
+	return c.rpc.Blob.Get(ctx, height, namespace, commitment)
+}
+
+// Get extended Celestia headers for a specific height
+func (c *OpenRPC) GetHeaders(ctx context.Context, height uint64) (*header.ExtendedHeader, error) {
+	return c.rpc.Header.GetByHeight(ctx, height)
+}
+
+// Get extended Celestia headers for a specific height
+func (c *OpenRPC) Included(ctx context.Context, height uint64, namespace share.Namespace, proof *blob.Proof, commitment blob.Commitment) (bool, error) {
+	return c.rpc.Blob.Included(ctx, height, namespace, proof, commitment)
 }
