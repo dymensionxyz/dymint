9c52ff9ad5505ec85013782cda1c8b66214fd760
Michael Tsitrin <michael@dymension.xyz>
2024-04-07 16:27:46 +0300

    moved config tests

--- config/config_test.go
+++ config/config_test.go
@@ -1,9 +1,10 @@
-package config
+package config_test
 
 import (
 	"testing"
 	"time"
 
+	"github.com/dymensionxyz/dymint/config"
 	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/assert"
 )
@@ -13,20 +14,20 @@ func TestViperAndCobra(t *testing.T) {
 	assert := assert.New(t)
 
 	cmd := &cobra.Command{}
-	AddNodeFlags(cmd)
+	config.AddNodeFlags(cmd)
 
 	dir := t.TempDir()
-	nc := DefaultConfig("", "")
-	EnsureRoot(dir, nc)
+	nc := config.DefaultConfig("", "")
+	config.EnsureRoot(dir, nc)
 
-	assert.NoError(cmd.Flags().Set(flagAggregator, "true"))
-	assert.NoError(cmd.Flags().Set(flagDALayer, "foobar"))
-	assert.NoError(cmd.Flags().Set(flagDAConfig, `{"json":true}`))
-	assert.NoError(cmd.Flags().Set(flagBlockTime, "1234s"))
-	assert.NoError(cmd.Flags().Set(flagEmptyBlocksMaxTime, "2000s"))
-	assert.NoError(cmd.Flags().Set(flagBatchSubmitMaxTime, "3000s"))
-	assert.NoError(cmd.Flags().Set(flagNamespaceID, "0102030405060708"))
-	assert.NoError(cmd.Flags().Set(flagBlockBatchSize, "10"))
+	assert.NoError(cmd.Flags().Set(config.FlagAggregator, "true"))
+	assert.NoError(cmd.Flags().Set(config.FlagDALayer, "foobar"))
+	assert.NoError(cmd.Flags().Set(config.FlagDAConfig, `{"json":true}`))
+	assert.NoError(cmd.Flags().Set(config.FlagBlockTime, "1234s"))
+	assert.NoError(cmd.Flags().Set(config.FlagEmptyBlocksMaxTime, "2000s"))
+	assert.NoError(cmd.Flags().Set(config.FlagBatchSubmitMaxTime, "3000s"))
+	assert.NoError(cmd.Flags().Set(config.FlagNamespaceID, "0102030405060708"))
+	assert.NoError(cmd.Flags().Set(config.FlagBlockBatchSize, "10"))
 
 	assert.NoError(nc.GetViperConfig(cmd, dir))
 
