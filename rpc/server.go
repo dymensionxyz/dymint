80e145fd9dcc45acd8cab654d22cd351625548b3
Michael Tsitrin <michael@dymension.xyz>
2024-04-15 00:09:34 +0300

    set const ReadHeaderTimeout to dymint RPC server

--- rpc/server.go
+++ rpc/server.go
@@ -25,6 +25,10 @@ import (
 	"github.com/dymensionxyz/dymint/utils"
 )
 
+const (
+	ReadHeaderTimeout = 5 * time.Second
+)
+
 // Server handles HTTP and JSON-RPC requests, exposing Tendermint-compatible API.
 type Server struct {
 	*service.BaseService
@@ -176,7 +180,10 @@ func (s *Server) startRPC() error {
 
 func (s *Server) serve(listener net.Listener, handler http.Handler) error {
 	s.Logger.Info("serving HTTP", "listen address", listener.Addr())
-	s.server = http.Server{Handler: handler} //#nosec
+	s.server = http.Server{
+		Handler:           handler,
+		ReadHeaderTimeout: ReadHeaderTimeout,
+	}
 	if s.config.TLSCertFile != "" && s.config.TLSKeyFile != "" {
 		return s.server.ServeTLS(listener, s.config.CertFile(), s.config.KeyFile())
 	}
