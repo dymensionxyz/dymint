d7dc81e14be4d3599fff342479abd613c005cafe
zale144 <aleksandar.sukovic@gmail.com>
2024-04-15 12:25:31 +0200

    Add missing config validation

--- config/config_test.go
+++ config/config_test.go
@@ -1,11 +1,15 @@
 package config
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
 	"github.com/spf13/cobra"
 	"github.com/stretchr/testify/assert"
+
+	"github.com/dymensionxyz/dymint/da/grpc"
+	"github.com/dymensionxyz/dymint/settlement"
 )
 
 func TestViperAndCobra(t *testing.T) {
@@ -38,4 +42,205 @@ func TestViperAndCobra(t *testing.T) {
 	assert.Equal(uint64(10), nc.BlockBatchSize)
 }
 
-//TODO: check invalid config
+func TestNodeConfig_Validate(t *testing.T) {
+	tests := []struct {
+		name     string
+		malleate func(*NodeConfig)
+
+		wantErr assert.ErrorAssertionFunc
+	}{
+		{
+			name:    "full config",
+			wantErr: assert.NoError,
+		}, {
+			name: "missing block time",
+			malleate: func(nc *NodeConfig) {
+				nc.BlockTime = 0
+			},
+			wantErr: assert.Error,
+		}, {
+			name: "missing empty blocks max time",
+			malleate: func(nc *NodeConfig) {
+				nc.EmptyBlocksMaxTime = -1
+			},
+			wantErr: assert.Error,
+		}, {
+			name: "missing batch submit max time",
+			malleate: func(nc *NodeConfig) {
+				nc.BlockManagerConfig.BatchSubmitMaxTime = 0
+			},
+			wantErr: assert.Error,
+		}, {
+			name: "empty_blocks_max_time not greater than block_time",
+			malleate: func(nc *NodeConfig) {
+				nc.BlockManagerConfig.EmptyBlocksMaxTime = 1
+				nc.BlockManagerConfig.BlockTime = 2
+			},
+			wantErr: assert.Error,
+		}, {
+			name: "batch_submit_max_time not greater than block_time",
+			malleate: func(nc *NodeConfig) {
+				nc.BlockManagerConfig.BatchSubmitMaxTime = 1
+				nc.BlockManagerConfig.BlockTime = 2
+			},
+			wantErr: assert.Error,
+		}, {
+			name: "missing block batch size",
+			malleate: func(nc *NodeConfig) {
+				nc.BlockManagerConfig.BlockBatchSize = 0
+			},
+			wantErr: assert.Error,
+		}, {
+			name: "missing block batch max size bytes",
+			malleate: func(nc *NodeConfig) {
+				nc.BlockManagerConfig.BlockBatchMaxSizeBytes = 0
+			},
+			wantErr: assert.Error,
+		}, {
+			name: "missing gossiped blocks cache size",
+			malleate: func(nc *NodeConfig) {
+				nc.BlockManagerConfig.GossipedBlocksCacheSize = 0
+			},
+			wantErr: assert.Error,
+		}, {
+			name: "empty settlement layer",
+			malleate: func(nc *NodeConfig) {
+				nc.SettlementLayer = ""
+			},
+			wantErr: assert.Error,
+		}, {
+			name: "settlement: provide both fees and gas prices",
+			malleate: func(nc *NodeConfig) {
+				nc.SettlementConfig.GasPrices = "1"
+				nc.SettlementConfig.GasFees = "1"
+			},
+			wantErr: assert.Error,
+		}, {
+			name: "settlement: provide neither fees nor gas prices",
+			malleate: func(nc *NodeConfig) {
+				nc.SettlementConfig.GasPrices = ""
+				nc.SettlementConfig.GasFees = ""
+			},
+			wantErr: assert.Error,
+		}, {
+			name: "settlement: missing rollapp id",
+			malleate: func(nc *NodeConfig) {
+				nc.SettlementConfig.RollappID = ""
+			},
+			wantErr: assert.Error,
+		}, {
+			name: "settlement: mock",
+			malleate: func(nc *NodeConfig) {
+				nc.SettlementLayer = "mock"
+			},
+			wantErr: assert.NoError,
+		}, {
+			name: "DALayer: empty",
+			malleate: func(nc *NodeConfig) {
+				nc.DALayer = ""
+			},
+			wantErr: assert.Error,
+		}, {
+			name: "DALayer: mock",
+			malleate: func(nc *NodeConfig) {
+				nc.DALayer = "mock"
+			},
+			wantErr: assert.NoError,
+		}, {
+			name: "DAConfig: empty",
+			malleate: func(nc *NodeConfig) {
+				nc.DAConfig = ""
+			},
+			wantErr: assert.Error,
+		}, {
+			name: "DAGrpc.Host empty",
+			malleate: func(nc *NodeConfig) {
+				nc.DAGrpc.Host = ""
+			},
+			wantErr: assert.Error,
+		}, {
+			name: "DAGrpc.Port 0",
+			malleate: func(nc *NodeConfig) {
+				nc.DAGrpc.Port = 0
+			},
+			wantErr: assert.Error,
+		}, {
+			name: "instrumentation: missing prometheus listen addr",
+			malleate: func(nc *NodeConfig) {
+				nc.Instrumentation.PrometheusListenAddr = ""
+			},
+			wantErr: assert.Error,
+		}, {
+			name: "instrumentation: prometheus enabled, but listen addr empty",
+			malleate: func(nc *NodeConfig) {
+				nc.Instrumentation.Prometheus = true
+				nc.Instrumentation.PrometheusListenAddr = ""
+			},
+			wantErr: assert.Error,
+		}, {
+			name: "instrumentation: prometheus disabled, but listen addr not empty",
+			malleate: func(nc *NodeConfig) {
+				nc.Instrumentation.Prometheus = false
+				nc.Instrumentation.PrometheusListenAddr = "localhost:9090"
+			},
+			wantErr: assert.Error,
+		}, {
+			name: "instrumentation: prometheus disabled, listen addr empty",
+			malleate: func(nc *NodeConfig) {
+				nc.Instrumentation.Prometheus = false
+				nc.Instrumentation.PrometheusListenAddr = ""
+			},
+			wantErr: assert.NoError,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			nc := fullNodeConfig()
+			if tt.malleate != nil {
+				tt.malleate(&nc)
+			}
+			tt.wantErr(t, nc.Validate(), fmt.Sprintf("Validate()"))
+		})
+	}
+}
+
+func fullNodeConfig() NodeConfig {
+	return NodeConfig{
+		BlockManagerConfig: BlockManagerConfig{
+			BlockTime:               1 * time.Second,
+			EmptyBlocksMaxTime:      2 * time.Second,
+			BatchSubmitMaxTime:      1 * time.Second,
+			NamespaceID:             "test",
+			BlockBatchSize:          1,
+			BlockBatchMaxSizeBytes:  1,
+			GossipedBlocksCacheSize: 1,
+		},
+		DALayer:         "celestia",
+		DAConfig:        "da-config",
+		SettlementLayer: "dymension",
+		SettlementConfig: settlement.Config{
+			KeyringBackend: "test",
+			NodeAddress:    "http://localhost:26657",
+			KeyringHomeDir: "/tmp/keyring-test",
+			DymAccountName: "test",
+			RollappID:      "test_123-1",
+			GasLimit:       120,
+			GasPrices:      "0.025stake",
+			GasFees:        "",
+			ProposerPubKey: "test",
+			SLGrpc: settlement.GrpcConfig{
+				Host:        "localhost",
+				Port:        9090,
+				RefreshTime: 1,
+			},
+		},
+		Instrumentation: &InstrumentationConfig{
+			Prometheus:           true,
+			PrometheusListenAddr: "localhost:9090",
+		},
+		DAGrpc: grpc.Config{
+			Host: "localhost",
+			Port: 9090,
+		},
+	}
+}
