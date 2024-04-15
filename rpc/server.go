da2ff94bcdd064109da703fa885609846a94e180
zale144 <aleksandar.sukovic@gmail.com>
2024-04-15 18:37:07 +0200

    fix(block): Only register `nodeHealthStatusHandler` for sequencer (#683)

--- rpc/server.go
+++ rpc/server.go
@@ -166,7 +166,7 @@ func (s *Server) startRPC() error {
 	// Start HTTP server
 	go func() {
 		err := s.serve(listener, handler)
-		if err != http.ErrServerClosed {
+		if !errors.Is(err, http.ErrServerClosed) {
 			s.Logger.Error("while serving HTTP", "error", err)
 		}
 	}()
@@ -176,7 +176,7 @@ func (s *Server) startRPC() error {
 
 func (s *Server) serve(listener net.Listener, handler http.Handler) error {
 	s.Logger.Info("serving HTTP", "listen address", listener.Addr())
-	s.server = http.Server{Handler: handler} //#nosec
+	s.server = http.Server{Handler: handler} // #nosec
 	if s.config.TLSCertFile != "" && s.config.TLSKeyFile != "" {
 		return s.server.ServeTLS(listener, s.config.CertFile(), s.config.KeyFile())
 	}
