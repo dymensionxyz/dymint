d005deb01f864e4a9048e061774dc53d94b2ffb2
danwt <30197399+danwt@users.noreply.github.com>
2024-04-15 18:05:33 +0100

    add a todo

--- da/utils.go
+++ da/utils.go
@@ -6,6 +6,7 @@ import (
 	"github.com/tendermint/tendermint/libs/pubsub"
 )
 
+// TODO: I need to check callsites of this, and help it match up with what I did with settlement, and make sure the healthy=noerror thing is continued
 func SubmitBatchHealthEventHelper(pubsubServer *pubsub.Server, ctx context.Context, err error) (ResultSubmitBatch, error) {
 	err = pubsubServer.PublishWithEvents(
 		ctx,
