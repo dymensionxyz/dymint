a254b170a710184844533e565afb691bae4ceb48
danwt <30197399+danwt@users.noreply.github.com>
2024-04-30 11:53:54 +0100

    fix decimal scaling and add test, docstrings

--- utils/retry/funcs.go
+++ utils/retry/funcs.go
@@ -24,12 +24,16 @@ func WithInitialDelay(d time.Duration) Option {
 	}
 }
 
+// WithMaxDelay sets the maximum delay for the backoff. The delay will not exceed this value.
+// Set 0 to disable the maximum delay.
 func WithMaxDelay(d time.Duration) Option {
 	return func(b *Backoff) {
 		b.maxDelay = d
 	}
 }
 
+// WithGrowthFactor sets the growth factor for the backoff. The delay will be multiplied by this factor on each call to Delay.
+// The factor should be greater than 1.0
 func WithGrowthFactor(x float64) Option {
 	return func(b *Backoff) {
 		b.growthFactor = x
@@ -48,8 +52,17 @@ func NewBackoff(opts ...Option) *Backoff {
 	return ret
 }
 
+// Delay returns the current delay. The subsequent delay will be increased by the growth factor up to the maximum.
+func (b *Backoff) Delay() time.Duration {
+	ret := b.delay
+	b.delay = time.Duration(float64(b.delay) * b.growthFactor)
+	if b.maxDelay != 0 {
+		b.delay = min(b.delay, b.maxDelay)
+	}
+	return ret
+}
+
+// Sleep sleeps for the current delay. The subsequent delay will be increased by the growth factor up to the maximum.
 func (b *Backoff) Sleep() {
-	time.Sleep(b.delay)
-	b.delay *= time.Duration(b.growthFactor)
-	b.delay = min(b.delay, b.maxDelay)
+	time.Sleep(b.Delay())
 }
