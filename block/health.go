52dbf61943e292789d06baff1c7e3c0260726361
danwt <30197399+danwt@users.noreply.github.com>
2024-04-30 18:06:02 +0100

    need to fix multi cancel

--- block/health.go
+++ block/health.go
@@ -15,29 +15,28 @@ type nodeHealthErrorHandler struct {
 	shouldProduceBlocksCh                     chan bool
 
 	// cancel any scheduled pause that was set to happen in the future
-	cancelScheduledBlockPause []func()
+	cancelScheduledBlockPause func()
 }
 
 func makeNodeHealthErrorHandler(errorTolerance time.Duration) nodeHealthErrorHandler {
 	return nodeHealthErrorHandler{
 		shouldProduceBlocksUnhealthyNodeTolerance: errorTolerance,
 		shouldProduceBlocksCh:                     make(chan bool, 1),
+		cancelScheduledBlockPause:                 func() {},
 	}
 }
 
 // handle must not be called concurrently
 func (h *nodeHealthErrorHandler) handle(err error) {
+	h.cancelScheduledBlockPause()
+
 	if err == nil {
 		// everything is fine!
-
-		for _, cancel := range h.cancelScheduledBlockPause {
-			cancel()
-		}
 		h.shouldProduceBlocksCh <- true // cancel any existing pause in production (NOTE: this line must go 2nd)
 		return
 	}
 
-	h.cancelScheduledBlockPause = append(h.cancelScheduledBlockPause, utime.CancellableAfterFunc(h.shouldProduceBlocksUnhealthyNodeTolerance, func() {
+	h.cancelScheduledBlockPause = utime.CancellableAfterFunc(h.shouldProduceBlocksUnhealthyNodeTolerance, func() {
 		h.shouldProduceBlocksCh <- false
-	}))
+	})
 }
