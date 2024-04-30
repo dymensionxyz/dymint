d98ee6954695d71c99fb553441fd523999ad8704
danwt <30197399+danwt@users.noreply.github.com>
2024-04-30 15:21:51 +0100

    comment

--- block/manager.go
+++ block/manager.go
@@ -61,6 +61,8 @@ type Manager struct {
 	// how long between a transition (node healthy -> unhealthy) before pausing block production.
 	// if a node is unhealthy for longer than this, we stop producing blocks
 	// if it becomes healthy again, the timer is cancelled
+	// note: in future, we could add the inverse for (node unhealthy -> healthy) to start producing
+	//       blocks again, but for now we will do that instantly
 	shouldProduceBlocksUnhealthyNodeTolerance time.Duration
 	shouldProduceBlocksCh                     chan bool
 	produceEmptyBlockCh                       chan bool
