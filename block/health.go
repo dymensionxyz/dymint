041aab718a12233304e785f6360f2377c06991f3
danwt <30197399+danwt@users.noreply.github.com>
2024-04-30 16:02:24 +0100

    simplify test

--- block/health.go
+++ block/health.go
@@ -1,43 +1,43 @@
 package block
 
 import (
+	"sync/atomic"
 	"time"
 )
 
 type nodeHealthErrorHandler struct {
 	// how long between a transition (node healthy -> unhealthy) before pausing block production.
 	// if a node is unhealthy for longer than this, we stop producing blocks
-	// if it becomes healthy again, the timer is cancelled
 	// note: in future, we could add the inverse for (node unhealthy -> healthy) to start producing
 	//       blocks again, but for now we will do that instantly
 	shouldProduceBlocksUnhealthyNodeTolerance time.Duration
 	shouldProduceBlocksCh                     chan bool
 
-	cancelFuturePause func()
+	// Something we will send on the channel in future
+	futureValueToSend atomic.Bool
 }
 
 func makeNodeHealthErrorHandler(errorTolerance time.Duration) nodeHealthErrorHandler {
 	return nodeHealthErrorHandler{
 		shouldProduceBlocksUnhealthyNodeTolerance: errorTolerance,
 		shouldProduceBlocksCh:                     make(chan bool, 1),
-		cancelFuturePause:                         func() {},
 	}
 }
 
 func (h *nodeHealthErrorHandler) handle(err error) {
-	if err == nil { // everything is fine!
-		if h.cancelFuturePause != nil {
-			// if we were unhealthy recently, make sure we dont stop producing blocks
-			h.cancelFuturePause()
-		}
-		h.shouldProduceBlocksCh <- true
+	/*
+		This implementation ended up being simpler than trying to use timer cancelling.
+	*/
+
+	if err == nil {
+		// everything is fine!
+		h.futureValueToSend.Store(true) // cancel any scheduled pause in production
+		h.shouldProduceBlocksCh <- true // cancel any existing pause in production
 		return
 	}
 
-	t := time.AfterFunc(h.shouldProduceBlocksUnhealthyNodeTolerance, func() {
-		h.shouldProduceBlocksCh <- false
+	h.futureValueToSend.Store(false)
+	time.AfterFunc(h.shouldProduceBlocksUnhealthyNodeTolerance, func() {
+		h.shouldProduceBlocksCh <- h.futureValueToSend.Load()
 	})
-	h.cancelFuturePause = func() {
-		t.Stop()
-	}
 }
--- block/health_test.go
+++ block/health_test.go
@@ -1,10 +1,34 @@
 package block
 
 import (
+	"errors"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestFoo(t *testing.T) {
-	h := makeNodeHealthErrorHandler(time.Second)
+	t.Run("healthy", func(t *testing.T) {
+		h := makeNodeHealthErrorHandler(time.Millisecond * 100)
+
+		h.handle(nil)
+		ok := <-h.shouldProduceBlocksCh
+		require.True(t, ok)
+	})
+	t.Run("unhealthy", func(t *testing.T) {
+		h := makeNodeHealthErrorHandler(time.Millisecond * 100)
+
+		h.handle(errors.New("foo"))
+		ok := <-h.shouldProduceBlocksCh
+		require.False(t, ok)
+	})
+	t.Run("healthy overrides unhealthy", func(t *testing.T) {
+		h := makeNodeHealthErrorHandler(time.Millisecond * 100)
+
+		h.handle(errors.New("foo"))
+		h.handle(nil) // healthy overrides unhealthy
+		ok := <-h.shouldProduceBlocksCh
+		require.True(t, ok)
+	})
 }
