2c643936ea06658c3507251ecdf628bf01b76222
danwt <30197399+danwt@users.noreply.github.com>
2024-05-01 15:19:51 +0100

    fix test

--- da/celestia/config_test.go
+++ da/celestia/config_test.go
@@ -2,70 +2,55 @@ package celestia
 
 import (
 	"encoding/json"
-	"fmt"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/require"
 
 	uretry "github.com/dymensionxyz/dymint/utils/retry"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func mustMarshal(v any) []byte {
-	bz, _ := json.Marshal(v)
-	return bz
-}
+func TestCreateConfig(t *testing.T) {
+	mustMarshal := func(v any) []byte {
+		bz, _ := json.Marshal(v)
+		return bz
+	}
 
-func Test_createConfig(t *testing.T) {
-	type args struct {
-		bz []byte
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantC   Config
-		wantErr assert.ErrorAssertionFunc
-	}{
-		{
-			name: "empty",
-			args: args{
-				bz: mustMarshal(Config{
-					BaseURL:       TestConfig.BaseURL,
-					AppNodeURL:    TestConfig.AppNodeURL,
-					Timeout:       TestConfig.Timeout,
-					Fee:           0,
-					GasPrices:     42,
-					GasAdjustment: 42,
-					GasLimit:      42,
-					Backoff:       uretry.NewBackoffConfig(),
-					RetryAttempts: 10,
-					RetryDelay:    10 * time.Second,
-					,
-				}),
-			},
-			wantC: Config{
-				BaseURL:       TestConfig.BaseURL,
-				AppNodeURL:    TestConfig.AppNodeURL,
-				Timeout:       TestConfig.Timeout,
-				Fee:           0,
-				GasPrices:     42,
-				GasAdjustment: 42,
-				GasLimit:      42,
-				Backoff:       uretry.NewBackoffConfig(),
-				RetryAttempts: 10,
-				RetryDelay:    10 * time.Second,
-				NamespaceID:   TestConfig.NamespaceID,
-			},
-			wantErr: assert.NoError,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			gotC, err := createConfig(tt.args.bz)
-			if !tt.wantErr(t, err, fmt.Sprintf("createConfig(%v)", tt.args.bz)) {
-				return
-			}
-			assert.Equalf(t, tt.wantC, gotC, "createConfig(%v)", tt.args.bz)
-		})
-	}
+	t.Run("simple", func(t *testing.T) {
+		c := Config{
+			BaseURL:       TestConfig.BaseURL,
+			AppNodeURL:    TestConfig.AppNodeURL,
+			Timeout:       TestConfig.Timeout,
+			Fee:           0,
+			GasPrices:     42,
+			GasAdjustment: 42,
+			GasLimit:      42,
+			Backoff:       uretry.NewBackoffConfig(uretry.WithGrowthFactor(1.65)),
+			RetryAttempts: 10,
+			RetryDelay:    10 * time.Second,
+		}
+		bz := mustMarshal(c)
+		gotC, err := createConfig(bz)
+		require.NoError(t, err)
+		assert.Equal(t, c.Backoff, gotC.Backoff)
+		assert.Equal(t, c.RetryAttempts, gotC.RetryAttempts)
+		assert.Equal(t, c.RetryDelay, gotC.RetryDelay)
+	})
+	t.Run("no backoff", func(t *testing.T) {
+		c := Config{
+			BaseURL:       TestConfig.BaseURL,
+			AppNodeURL:    TestConfig.AppNodeURL,
+			Timeout:       TestConfig.Timeout,
+			Fee:           0,
+			GasPrices:     42,
+			GasAdjustment: 42,
+			GasLimit:      42,
+		}
+		bz := mustMarshal(c)
+		gotC, err := createConfig(bz)
+		require.NoError(t, err)
+		assert.Equal(t, defaultSubmitBackoff, gotC.Backoff)
+	})
 }
