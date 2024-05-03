fad015ee26656d43c24cc58ccb804a7fa13d6209
danwt <30197399+danwt@users.noreply.github.com>
2024-05-03 11:07:54 +0100

    remove debug log

--- block/retriever.go
+++ block/retriever.go
@@ -39,7 +39,6 @@ func (m *Manager) RetrieveLoop(ctx context.Context) {
 func (m *Manager) syncUntilTarget(targetHeight uint64) error {
 	for currH := m.Store.Height(); currH < targetHeight; currH = m.Store.Height() {
 
-		m.logger.Error(fmt.Sprintf("Current height: %d, target height: %d\n, store height: %d", currH, targetHeight, m.Store.Height()))
 		// It's important that we query the state index before fetching the batch, rather
 		// than e.g. keep it and increment it, because we might be concurrently applying blocks
 		// and may require a higher index than expected.
