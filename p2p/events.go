851b312620233a9fb1abe55214a678322e7b0c68
Sergi Rene <sergi@datahop.network>
2024-05-05 09:16:49 +0200

    fix(p2p): validating gossiped block is created by the proposer (#737)
    Co-authored-by: github-actions <github-actions@github.com>
    Co-authored-by: Michael Tsitrin <michael@dymension.xyz>

--- p2p/events.go
+++ p2p/events.go
@@ -1,8 +1,6 @@
 package p2p
 
 import (
-	"github.com/dymensionxyz/dymint/p2p/pb"
-	"github.com/dymensionxyz/dymint/types"
 	uevent "github.com/dymensionxyz/dymint/utils/event"
 )
 
@@ -18,64 +16,6 @@ const (
 const (
 	EventNewGossipedBlock = "NewGossipedBlock"
 )
-
-/* -------------------------------------------------------------------------- */
-/*                                 Event Data                                 */
-/* -------------------------------------------------------------------------- */
-
-// GossipedBlock defines the struct of the event data for the GossipedBlock
-type GossipedBlock struct {
-	// Block is the block that was gossiped
-	Block types.Block
-	// Commit is the commit that was gossiped
-	Commit types.Commit
-}
-
-// MarshalBinary encodes GossipedBlock into binary form and returns it.
-func (e *GossipedBlock) MarshalBinary() ([]byte, error) {
-	return e.ToProto().Marshal()
-}
-
-// UnmarshalBinary decodes binary form of GossipedBlock into object.
-func (e *GossipedBlock) UnmarshalBinary(data []byte) error {
-	var pbGossipedBlock pb.GossipedBlock
-	err := pbGossipedBlock.Unmarshal(data)
-	if err != nil {
-		return err
-	}
-	err = e.FromProto(&pbGossipedBlock)
-	return err
-}
-
-// ToProto converts Data into protobuf representation and returns it.
-func (e *GossipedBlock) ToProto() *pb.GossipedBlock {
-	return &pb.GossipedBlock{
-		Block:  e.Block.ToProto(),
-		Commit: e.Commit.ToProto(),
-	}
-}
-
-// FromProto fills GossipedBlock with data from its protobuf representation.
-func (e *GossipedBlock) FromProto(other *pb.GossipedBlock) error {
-	if err := e.Block.FromProto(other.Block); err != nil {
-		return err
-	}
-	if err := e.Commit.FromProto(other.Commit); err != nil {
-		return err
-	}
-	return nil
-}
-
-// Validate run basic validation on the gossiped block
-func (e *GossipedBlock) Validate() error {
-	if err := e.Block.ValidateBasic(); err != nil {
-		return err
-	}
-	if err := e.Commit.ValidateBasic(); err != nil {
-		return err
-	}
-	return nil
-}
 
 /* -------------------------------------------------------------------------- */
 /*                                   Queries                                  */
