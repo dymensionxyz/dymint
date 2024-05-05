4a0b80d95a74a88e4a59cb97e20add1671489bbb
Sergi Rene <sergi@datahop.network>
2024-05-05 09:16:49 +0200

    fix(p2p): validating gossiped block is created by the proposer (#737)
    Co-authored-by: github-actions <github-actions@github.com>
    Co-authored-by: Michael Tsitrin <michael@dymension.xyz>

--- types/errors.go
+++ types/errors.go
@@ -3,8 +3,9 @@ package types
 import "errors"
 
 var (
-	ErrInvalidSignature   = errors.New("invalid signature")
-	ErrNoStateFound       = errors.New("no state found")
-	ErrSkippedEmptyBlock  = errors.New("skipped empty block")
-	ErrInvalidBlockHeight = errors.New("invalid block height")
+	ErrInvalidSignature      = errors.New("invalid signature")
+	ErrNoStateFound          = errors.New("no state found")
+	ErrSkippedEmptyBlock     = errors.New("skipped empty block")
+	ErrInvalidBlockHeight    = errors.New("invalid block height")
+	ErrInvalidHeaderDataHash = errors.New("header not matching block data")
 )
