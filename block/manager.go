5f1c00f30e8ce20998c49e65e274616eb4416323
Sergi Rene <sergi@dymension.xyz>
2024-05-13 22:20:37 +0200

    remove wait

--- block/manager.go
+++ block/manager.go
@@ -169,10 +169,6 @@ func (m *Manager) Start(ctx context.Context) error {
 		go m.ProduceBlockLoop(ctx)
 		go m.SubmitLoop(ctx)
 	} else {
-		fmt.Println("waiting")
-		<-m.DAClient.Started()
-		fmt.Println("started")
-
 		go uevent.MustSubscribe(ctx, m.Pubsub, "applyGossipedBlocksLoop", p2p.EventQueryNewNewGossipedBlock, m.onNewGossipedBlock, m.logger)
 		go m.RetrieveLoop(ctx)
 		go m.SyncToTargetHeightLoop(ctx)
