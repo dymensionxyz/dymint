0c7e51743104453c99d0b38a7be6e2836a04d9f0
Daniel T <30197399+danwt@users.noreply.github.com>
2024-06-03 14:36:29 +0100

    patch for mtsitrin 884: use errors.Is (#895)

--- settlement/dymension/types.go
+++
@@ -1,8 +0,0 @@
-package dymension
-
-import (
-	dymension "github.com/dymensionxyz/dymension/v3/x/rollapp/types"
-)
-
-// if this error is returned, the batch is already submitted
-var ErrBatchAlreadySubmitted = dymension.ErrWrongBlockHeight.Error()
