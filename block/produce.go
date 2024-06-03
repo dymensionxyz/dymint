8db057253f7a456f237970ad1a73f4fdf9bf8ccd
Michael Tsitrin <114929630+mtsitrin@users.noreply.github.com>
2024-06-03 14:59:53 +0300

    fix(block manage): reducing batch overhead size when reaping Txs from mempool (#886)

--- block/produce.go
+++ block/produce.go
@@ -128,7 +128,9 @@ func (m *Manager) produceBlock(allowEmpty bool) (*types.Block, *types.Commit, er
 	} else if !errors.Is(err, gerr.ErrNotFound) {
 		return nil, nil, fmt.Errorf("load block: height: %d: %w: %w", newHeight, err, ErrNonRecoverable)
 	} else {
-		block = m.Executor.CreateBlock(newHeight, lastCommit, lastHeaderHash, m.State, m.Conf.BlockBatchMaxSizeBytes)
+		// limit to the max block data, so we don't create a block that is too big to fit in a batch
+		maxBlockDataSize := uint64(float64(m.Conf.BlockBatchMaxSizeBytes) * types.MaxBlockSizeAdjustment)
+		block = m.Executor.CreateBlock(newHeight, lastCommit, lastHeaderHash, m.State, maxBlockDataSize)
 		if !allowEmpty && len(block.Data.Txs) == 0 {
 			return nil, nil, fmt.Errorf("%w: %w", types.ErrSkippedEmptyBlock, ErrRecoverable)
 		}
