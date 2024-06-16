18939de4d6d0945beedd3c710006ad487ce3a75d
Sergi Rene <sergi@dymension.xyz>
2024-06-16 19:24:16 +0200

    adv fix

--- p2p/client_test.go
+++ p2p/client_test.go
@@ -7,10 +7,12 @@ import (
 	"testing"
 	"time"
 
+	"github.com/ipfs/go-cid"
 	"github.com/ipfs/go-datastore"
 	"github.com/libp2p/go-libp2p/core/crypto"
 	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/multiformats/go-multiaddr"
+	mh "github.com/multiformats/go-multihash"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
@@ -133,6 +135,112 @@ func TestGossiping(t *testing.T) {
 
 	// wait for clients that should receive Tx
 	wg.Wait()
+}
+
+func TestAdvertiseBlock(t *testing.T) {
+	assert := assert.New(t)
+	logger := log.TestingLogger()
+
+	ctx := context.Background()
+
+	// required for tx validator
+	assertRecv := func(tx *p2p.GossipMessage) bool {
+		return true
+	}
+
+	// Create a cid manually by specifying the 'prefix' parameters
+	pref := &cid.Prefix{
+		Codec:    cid.DagProtobuf,
+		MhLength: -1,
+		MhType:   mh.SHA2_256,
+		Version:  1,
+	}
+
+	// And then feed it some data
+	expectedCid, err := pref.Sum([]byte("test"))
+	assert.NoError(err)
+
+	// validators required
+	validators := []p2p.GossipValidator{assertRecv, assertRecv, assertRecv, assertRecv, assertRecv}
+
+	// network connections topology: 3<->1<->0<->2<->4
+	clients := testutil.StartTestNetwork(ctx, t, 5, map[int]testutil.HostDescr{
+		0: {Conns: []int{}, ChainID: "2"},
+		1: {Conns: []int{0}, ChainID: "1", RealKey: true},
+		2: {Conns: []int{0}, ChainID: "1", RealKey: true},
+		3: {Conns: []int{1}, ChainID: "2", RealKey: true},
+		4: {Conns: []int{2}, ChainID: "2", RealKey: true},
+	}, validators, logger)
+
+	// wait for clients to finish refreshing routing tables
+	clients.WaitForDHT()
+
+	// this sleep is required for pubsub to "propagate" subscription information
+	// TODO(tzdybal): is there a better way to wait for readiness?
+	time.Sleep(1 * time.Second)
+
+	// advertise cid for height 1
+	err = clients[4].AdvertiseBlock(ctx, 1, expectedCid)
+	assert.NoError(err)
+
+	// get cid for height 1
+	receivedCid, err := clients[0].GetBlockId(ctx, 1)
+	assert.NoError(err)
+	assert.Equal(expectedCid, receivedCid)
+
+}
+
+func TestGetBlock(t *testing.T) {
+	assert := assert.New(t)
+	logger := log.TestingLogger()
+
+	ctx := context.Background()
+
+	// required for tx validator
+	assertRecv := func(tx *p2p.GossipMessage) bool {
+		return true
+	}
+
+	// Create a cid manually by specifying the 'prefix' parameters
+	pref := &cid.Prefix{
+		Codec:    cid.DagProtobuf,
+		MhLength: -1,
+		MhType:   mh.SHA2_256,
+		Version:  1,
+	}
+
+	// And then feed it some data
+	expectedCid, err := pref.Sum([]byte("test"))
+	assert.NoError(err)
+
+	// validators required
+	validators := []p2p.GossipValidator{assertRecv, assertRecv, assertRecv, assertRecv, assertRecv}
+
+	// network connections topology: 3<->1<->0<->2<->4
+	clients := testutil.StartTestNetwork(ctx, t, 5, map[int]testutil.HostDescr{
+		0: {Conns: []int{}, ChainID: "2"},
+		1: {Conns: []int{0}, ChainID: "1", RealKey: true},
+		2: {Conns: []int{0}, ChainID: "1", RealKey: true},
+		3: {Conns: []int{1}, ChainID: "2", RealKey: true},
+		4: {Conns: []int{2}, ChainID: "2", RealKey: true},
+	}, validators, logger)
+
+	// wait for clients to finish refreshing routing tables
+	clients.WaitForDHT()
+
+	// this sleep is required for pubsub to "propagate" subscription information
+	// TODO(tzdybal): is there a better way to wait for readiness?
+	time.Sleep(1 * time.Second)
+
+	// advertise cid for height 1
+	err = clients[4].AdvertiseBlock(ctx, 1, expectedCid)
+	assert.NoError(err)
+
+	// get cid for height 1
+	receivedCid, err := clients[0].GetBlockId(ctx, 1)
+	assert.NoError(err)
+	assert.Equal(expectedCid, receivedCid)
+
 }
 
 func TestSeedStringParsing(t *testing.T) {
