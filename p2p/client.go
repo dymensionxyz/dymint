6d8112ee375be45567855ee3b6564a256882f3bb
Sergi Rene <sergi@dymension.xyz>
2024-06-19 12:16:00 +0200

    lint fix

--- p2p/client.go
+++ p2p/client.go
@@ -525,7 +525,6 @@ func (c *Client) SetLatestSeenHeight(height uint64) {
 
 // already applied height set as a min height to retrieved using block-sync
 func (c *Client) SetAppliedHeight(height uint64) {
-
 	defer c.blockAppliedHeightMu.Unlock()
 	c.blockAppliedHeightMu.Lock()
 
