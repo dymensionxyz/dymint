bb07915bc41ddeebf1df2b3edfd1ab48464ffbfc
keruch <verendeev.kirill.2000@gmail.com>
2024-06-27 21:03:10 +0200

    feat(da): SubmitBatch method

--- da/interchain/config.go
+++ da/interchain/config.go
@@ -1,25 +1,19 @@
 package interchain
 
 import (
-	"github.com/cosmos/cosmos-sdk/client"
-	"github.com/cosmos/cosmos-sdk/codec"
+	interchainda "github.com/dymensionxyz/dymint/types/pb/interchain_da"
 )
 
 type DAConfig struct {
-	ChainID        string                   `mapstructure:"chain_id"`  // The chain ID of the DA chain
-	ClientID       string                   `mapstructure:"client_id"` // This is the IBC client ID on Dymension hub for the DA chain
-	KeyringBackend string                   `mapstructure:"keyring_backend"`
-	KeyringHomeDir string                   `mapstructure:"keyring_home_dir"`
-	AddressPrefix  string                   `mapstructure:"da_address_prefix"`
-	AccountName    string                   `mapstructure:"da_account_name"`
-	NodeAddress    string                   `mapstructure:"da_node_address"`
-	GasLimit       uint64                   `mapstructure:"da_gas_limit"`
-	GasPrices      string                   `mapstructure:"da_gas_prices"`
-	GasFees        string                   `mapstructure:"da_gas_fees"`
-	ChainParams    interchaindatypes.Params `mapstructure:"chain_params"` // The params of the DA chain
-}
-
-type EncodingConfig interface {
-	TxConfig() client.TxConfig
-	Codec() codec.Codec
+	ClientID       string              `mapstructure:"client_id"` // This is the IBC client ID on Dymension hub for the DA chain
+	ChainID        string              `mapstructure:"chain_id"`  // The chain ID of the DA chain
+	KeyringBackend string              `mapstructure:"keyring_backend"`
+	KeyringHomeDir string              `mapstructure:"keyring_home_dir"`
+	AddressPrefix  string              `mapstructure:"address_prefix"`
+	AccountName    string              `mapstructure:"account_name"`
+	NodeAddress    string              `mapstructure:"node_address"`
+	GasLimit       uint64              `mapstructure:"gas_limit"`
+	GasPrices      string              `mapstructure:"gas_prices"`
+	GasFees        string              `mapstructure:"gas_fees"`
+	DAParams       interchainda.Params `mapstructure:"da_params"`
 }
