111346f896610744156c831a52a4d1caa1db2d58
keruch <verendeev.kirill.2000@gmail.com>
2024-07-02 10:54:37 +0200

    feat(da): config tests

--- da/interchain/config.go
+++ da/interchain/config.go
@@ -13,22 +13,22 @@ import (
 )
 
 type DAConfig struct {
-	ClientID string `mapstructure:"client_id"` // IBC client ID between the Hub and DA layer
-	ChainID  string `mapstructure:"chain_id"`  // Chain ID of the DA layer
+	ClientID string `mapstructure:"client_id" json:"client_id,omitempty"` // IBC client ID between the Hub and DA layer
+	ChainID  string `mapstructure:"chain_id" json:"chain_id,omitempty"`   // Chain ID of the DA layer
 
-	KeyringBackend string              `mapstructure:"keyring_backend"`
-	KeyringHomeDir string              `mapstructure:"keyring_home_dir"`
-	AddressPrefix  string              `mapstructure:"address_prefix"`
-	AccountName    string              `mapstructure:"account_name"`
-	NodeAddress    string              `mapstructure:"node_address"`
-	GasLimit       uint64              `mapstructure:"gas_limit"`
-	GasPrices      string              `mapstructure:"gas_prices"`
-	GasFees        string              `mapstructure:"gas_fees"`
-	DAParams       interchainda.Params `mapstructure:"da_params"`
+	KeyringBackend string              `mapstructure:"keyring_backend" json:"keyring_backend,omitempty"`
+	KeyringHomeDir string              `mapstructure:"keyring_home_dir" json:"keyring_home_dir,omitempty"`
+	AddressPrefix  string              `mapstructure:"address_prefix" json:"address_prefix,omitempty"`
+	AccountName    string              `mapstructure:"account_name" json:"account_name,omitempty"`
+	NodeAddress    string              `mapstructure:"node_address" json:"node_address,omitempty"`
+	GasLimit       uint64              `mapstructure:"gas_limit" json:"gas_limit,omitempty"`
+	GasPrices      string              `mapstructure:"gas_prices" json:"gas_prices,omitempty"`
+	GasFees        string              `mapstructure:"gas_fees" json:"gas_fees,omitempty"`
+	DAParams       interchainda.Params `mapstructure:"da_params" json:"da_params"`
 
-	RetryMinDelay time.Duration `mapstructure:"retry_min_delay"`
-	RetryMaxDelay time.Duration `mapstructure:"retry_min_delay"`
-	RetryAttempts uint          `mapstructure:"retry_attempts"`
+	RetryMinDelay time.Duration `mapstructure:"retry_min_delay" json:"retry_min_delay,omitempty"`
+	RetryMaxDelay time.Duration `mapstructure:"retry_min_delay" json:"retry_max_delay,omitempty"`
+	RetryAttempts uint          `mapstructure:"retry_attempts" json:"retry_attempts,omitempty"`
 }
 
 func (c *DAConfig) Verify() error {
@@ -37,32 +37,30 @@ func (c *DAConfig) Verify() error {
 	}
 
 	if c.ChainID == "" {
-		return errors.New("missing Chain ID of the DA layer")
+		return errors.New("missing chain ID of the DA layer")
 	}
 
 	if c.KeyringBackend == "" {
-		return errors.New("missing Keyring Backend")
+		return errors.New("missing keyring backend")
 	}
 
 	if c.KeyringHomeDir == "" {
-		return errors.New("missing Keyring HomeDir")
+		return errors.New("missing keyring home dir")
 	}
 
 	if c.AddressPrefix == "" {
-		return errors.New("missing Address Prefix")
+		return errors.New("missing address prefix")
 	}
 
 	if c.AccountName == "" {
-		return errors.New("missing Account Name")
+		return errors.New("missing account name")
 	}
 
 	if c.NodeAddress == "" {
-		return errors.New("missing Node Address")
+		return errors.New("missing node address")
 	}
 
-	if c.GasLimit == 0 {
-		return errors.New("missing Gas Limit")
-	}
+	// GasLimit may be 0
 
 	if c.GasPrices == "" && c.GasFees == "" {
 		return errors.New("either gas prices or gas_prices are required")
@@ -75,15 +73,15 @@ func (c *DAConfig) Verify() error {
 	// DAParams are set during Init
 
 	if c.RetryMinDelay.Nanoseconds() == 0 {
-		return errors.New("missing Retry MinDelay")
+		return errors.New("missing retry min delay")
 	}
 
 	if c.RetryMaxDelay.Nanoseconds() == 0 {
-		return errors.New("missing Retry MaxDelay")
+		return errors.New("missing retry max delay")
 	}
 
 	if c.RetryAttempts == 0 {
-		return errors.New("missing Retry Attempts")
+		return errors.New("missing retry attempts")
 	}
 
 	return nil
@@ -93,7 +91,7 @@ func DefaultDAConfig() DAConfig {
 	home, _ := os.UserHomeDir()
 	keyringHomeDir := filepath.Join(home, ".simapp")
 	return DAConfig{
-		ClientID:       "",
+		ClientID:       "dym-interchain",
 		ChainID:        "interchain-da-test",
 		KeyringBackend: keyring.BackendTest,
 		KeyringHomeDir: keyringHomeDir,
