c4d1c4f053dddaee558c2dd90292f9ff6bdca003
danwt <30197399+danwt@users.noreply.github.com>
2024-07-18 14:03:56 +0100

    delete grey avail test

--- da/avail/avail_test.go
+++ da/avail/avail_test.go
@@ -20,96 +20,9 @@ const (
 	seed = "copper mother insect grunt blue cute tell side welcome domain border oxygen"
 )
 
-// FIXME(omritoptix): This test is currently not working as I couldn't find a way to mock the SubmitAndWatchExtrinsic function.
-// func TestSubmitBatch(t *testing.T) {
-// 	assert := assert.New(t)
-// 	require := require.New(t)
-// 	configBytes, err := json.Marshal(avail.Config{
-// 		Seed: seed,
-// 	})
-// 	require.NoError(err)
-// 	// Create mock clients
-// 	mockSubstrateApiClient := mocks.NewSubstrateApiI(t)
-// 	// Configure DALC options
-// 	options := []da.Option{
-// 		avail.WithClient(mockSubstrateApiClient),
-// 		avail.WithBatchRetryAttempts(1),
-// 		avail.WithBatchRetryDelay(1 * time.Second),
-// 		avail.WithTxInclusionTimeout(1 * time.Second),
-// 	}
-// 	// Subscribe to the health status event
-// 	pubsubServer := pubsub.NewServer()
-// 	pubsubServer.Start()
-// 	// HealthSubscription, err := pubsubServer.Subscribe(context.Background(), "testSubmitBatch", da.EventQueryDAHealthStatus)
-// 	assert.NoError(err)
-// 	// Start the DALC
-// 	dalc := avail.DataAvailabilityLayerClient{}
-// 	err = dalc.Init(configBytes, pubsubServer, nil, test.NewLogger(t), options...)
-// 	require.NoError(err)
-// 	err = dalc.Start()
-// 	require.NoError(err)
-// 	// Set the mock functions
-// 	metadata := availtypes.NewMetadataV14()
-// 	metadata.AsMetadataV14 = availtypes.MetadataV14{
-// 		Pallets: []availtypes.PalletMetadataV14{
-// 			{
-// 				Name:     "DataAvailability",
-// 				HasCalls: true,
-// 			},
-// 			{
-// 				Name:       "System",
-// 				HasStorage: true,
-// 				Storage: availtypes.StorageMetadataV14{
-// 					Prefix: "System",
-// 					Items: []availtypes.StorageEntryMetadataV14{
-// 						{
-// 							Name: "Account",
-// 							Type: availtypes.StorageEntryTypeV14{
-// 								IsPlainType: true,
-// 								IsMap:       true,
-// 								AsMap: availtypes.MapTypeV14{
-// 									Hashers: []availtypes.StorageHasherV10{
-// 										{
-// 											IsIdentity: true,
-// 										},
-// 									},
-// 								},
-// 							},
-// 						},
-// 					},
-// 				},
-// 			},
-// 		},
-// 		EfficientLookup: map[int64]*availtypes.Si1Type{
-// 			0: {
-// 				Def: availtypes.Si1TypeDef{
-// 					Variant: availtypes.Si1TypeDefVariant{
-// 						Variants: []availtypes.Si1Variant{
-// 							{
-// 								Name: "submit_data",
-// 							},
-// 						},
-// 					},
-// 				},
-// 			},
-// 		},
-// 	}
+// TODO: there was another test here, it should be brought back https://github.com/dymensionxyz/dymint/issues/970
 
-// 	mockSubstrateApiClient.On("GetMetadataLatest").Return(metadata, nil)
-// 	mockSubstrateApiClient.On("GetBlockHash", mock.Anything).Return(availtypes.NewHash([]byte("123")), nil)
-// 	mockSubstrateApiClient.On("GetRuntimeVersionLatest").Return(availtypes.NewRuntimeVersion(), nil)
-// 	mockSubstrateApiClient.On("GetStorageLatest", mock.Anything, mock.Anything).Return(true, nil)
-// 	mockSubstrateApiClient.On("SubmitAndWatchExtrinsic", mock.Anything).Return(nil, nil)
-// batch := &types.Batch{
-// 	StartHeight: 0,
-// 	EndHeight:   1,
-// }
-// res := dalc.SubmitBatch(batch)
-// assert.Equal(res.Code, da.StatusSuccess)
-
-// }
-
-// TestRetriveBatches tests the RetrieveBatches function manages
+// TestRetrieveBatches tests the RetrieveBatches function manages
 // to decode the batches from the block extrinsics and only returns
 // the batches relevant for our app id and method index.
 func TestRetrieveBatches(t *testing.T) {
