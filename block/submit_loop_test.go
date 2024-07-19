4cf9fcea0aabfb992ba0a1700a817991118a334d
danwt <30197399+danwt@users.noreply.github.com>
2024-07-19 16:04:39 +0100

    remove pending bytes re

--- block/submit_loop_test.go
+++ block/submit_loop_test.go
@@ -2,25 +2,40 @@ package block_test
 
 import (
 	"context"
-	"fmt"
 	"math/rand"
+	"sync"
 	"sync/atomic"
 	"testing"
 	"time"
 
 	"github.com/dymensionxyz/dymint/block"
+	"github.com/stretchr/testify/require"
 )
 
 type testArgs struct {
-	testDuration              time.Duration
-	batchSkew                 uint64
-	batchBytes                uint64
-	maxTime                   time.Duration
-	submitTime                time.Duration
-	produceTime               time.Duration
-	produceBytes              int
-	submissionHaltTime        time.Duration
-	submissionHaltProbability float64
+	testDuration              time.Duration // how long to run one instance of the test (should be short)
+	batchSkew                 uint64        // max number of batches to get ahead
+	batchBytes                uint64        // max number of bytes in a batch
+	maxTime                   time.Duration // maximum time to wait before submitting submissions
+	submitTime                time.Duration // how long it takes to submit a batch
+	produceTime               time.Duration // time between producing block
+	produceBytes              int           // range of how many bytes each block (+ commit) is
+	submissionHaltTime        time.Duration // how long to simulate batch submission failing/halting
+	submissionHaltProbability float64       // probability of submission failing and causing a temporary halt
+}
+
+func testSubmitLoop(t *testing.T,
+	args testArgs,
+) {
+	var wg sync.WaitGroup
+	for range 50 { // do multiple simulations in parallel
+		wg.Add(1)
+		go func() {
+			testSubmitLoopInner(t, args)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
 }
 
 func testSubmitLoopInner(
@@ -30,65 +45,76 @@ func testSubmitLoopInner(
 	ctx, cancel := context.WithTimeout(context.Background(), args.testDuration)
 	defer cancel()
 
-	c := make(chan int)
-
-	bz := atomic.Uint64{}
-
+	// returns a duration in [0.8,1.2] * d
 	approx := func(d time.Duration) time.Duration {
 		base := int(float64(d) * 0.8)
 		factor := int(float64(d) * 0.4)
 		return time.Duration(base + rand.Intn(factor))
 	}
 
-	// TODO: can pick random params, but need to be careful
-
-	produce := func() {
+	nProducedBytes := atomic.Uint64{} // tracking how many actual bytes have been produced but not submitted so far
+	producedBytesC := make(chan int)  // producer sends on here, and can be blocked by not consuming from here
+	go func() {                       // simulate block production
+		go func() { // another thread to check system properties
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				default:
+				}
+				// producer shall not get too far ahead
+				require.True(t, nProducedBytes.Load() < (args.batchSkew+1)*args.batchBytes)
+			}
+		}()
 		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+			}
 			time.Sleep(approx(args.produceTime))
-			x := rand.Intn(args.produceBytes)
-			after := bz.Add(uint64(x))
-			t.Log(fmt.Sprintf("actual pending bytes: %d", after))
-			c <- x
+			producedBytesC <- rand.Intn(args.produceBytes)
 		}
-	}
+	}()
 
-	submit := func(maxSize uint64) (uint64, error) {
+	lastSubmitTime := time.Time{}
+	submitBatch := func(maxSize uint64) (uint64, error) { // mock the batch submission
 		time.Sleep(approx(args.submitTime))
 		if rand.Float64() < args.submissionHaltProbability {
-			fmt.Println("submitter transient halt")
 			time.Sleep(args.submissionHaltTime)
 		}
-		y := rand.Intn(int(maxSize))
-		bz.Add(^uint64(y - 1))
-		return uint64(y), nil
-	}
+		consumed := rand.Intn(int(maxSize))
+		nProducedBytes.Add(^uint64(consumed - 1)) // subtract
 
-	go produce()
+		require.True(t, lastSubmitTime == time.Time{} || float64(time.Since(lastSubmitTime)) < 1.5*float64(args.maxTime))
+		lastSubmitTime = time.Now()
+
+		return uint64(consumed), nil
+	}
 
 	block.SubmitLoopInner(
 		ctx,
-		c,
+		producedBytesC,
 		args.batchSkew,
 		args.maxTime,
 		args.batchBytes,
-		submit,
+		submitBatch,
 	)
 }
 
 // Make sure the producer does not get too far ahead
 func TestSubmitLoopFastProducerHaltingSubmitter(t *testing.T) {
-	t.Skip()
-	testSubmitLoopInner(
+	testSubmitLoop(
 		t,
 		testArgs{
-			testDuration:              60 * time.Minute,
+			testDuration:              time.Second,
 			batchSkew:                 10,
 			batchBytes:                100,
-			maxTime:                   500 * time.Millisecond,
-			submitTime:                100 * time.Millisecond,
+			maxTime:                   50 * time.Millisecond,
+			submitTime:                2 * time.Millisecond,
 			produceBytes:              20,
-			produceTime:               100 * time.Millisecond,
-			submissionHaltTime:        30000 * time.Millisecond,
+			produceTime:               2 * time.Millisecond,
+			submissionHaltTime:        50 * time.Millisecond,
 			submissionHaltProbability: 0.01,
 		},
 	)
@@ -97,7 +123,7 @@ func TestSubmitLoopFastProducerHaltingSubmitter(t *testing.T) {
 // Make sure the timer works even if the producer is slow
 func TestSubmitLoopTimer(t *testing.T) {
 	t.Skip()
-	testSubmitLoopInner(
+	testSubmitLoop(
 		t,
 		testArgs{
 			testDuration: 60 * time.Second,
