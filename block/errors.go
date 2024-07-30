375e3e29d5fbc8cbc60d425f83b0284b2167cade
Sergi Rene <sergi@dymension.xyz>
2024-07-30 17:23:07 +0200

    move da error

--- block/errors.go
+++ block/errors.go
@@ -5,5 +5,4 @@ import "errors"
 var (
 	ErrNonRecoverable = errors.New("non recoverable")
 	ErrRecoverable    = errors.New("recoverable")
-	ErrWrongDA        = errors.New("DA in config not matching DA path")
 )
