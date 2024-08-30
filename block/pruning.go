86db79f9ed2fe56d3d930d837a9cb06b4f0f0ab5
Sergi Rene <sergi@dymension.xyz>
2024-08-30 13:15:12 +0200

    remove redundant logs

--- block/pruning.go
+++ block/pruning.go
@@ -9,7 +9,6 @@ import (
 
 func (m *Manager) PruneBlocks(retainHeight uint64) error {
 	if m.IsProposer() && m.NextHeightToSubmit() < retainHeight { // do not delete anything that we might submit in future
-		m.logger.Error("skipping block pruning. next height to submit is previous to retain_height.", "retain_height", retainHeight, "next_submit_height", m.NextHeightToSubmit())
 		return fmt.Errorf("skipping block pruning. next height to submit is previous to retain_height.t %d: next height to submit: %d: %w",
 			retainHeight,
 			m.NextHeightToSubmit(),
@@ -24,7 +23,6 @@ func (m *Manager) PruneBlocks(retainHeight uint64) error {
 
 	pruned, err := m.Store.PruneBlocks(m.State.BaseHeight, retainHeight)
 	if err != nil {
-		m.logger.Error("pruning dymint store", "retain_height", retainHeight, "err", err)
 		return fmt.Errorf("pruning dymint store: %w", err)
 	}
 
@@ -33,8 +31,7 @@ func (m *Manager) PruneBlocks(retainHeight uint64) error {
 	m.State.BaseHeight = retainHeight
 	_, err = m.Store.SaveState(m.State, nil)
 	if err != nil {
-		m.logger.Error("saving state.", "retain_height", retainHeight, "err", err)
-		return fmt.Errorf("save state: %w", err)
+		return fmt.Errorf("saving state: %w", err)
 	}
 
 	m.logger.Info("pruned blocks", "pruned", pruned, "retain_height", retainHeight)
