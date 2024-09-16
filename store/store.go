81d921af8577683793bb3e877134bf177c652f6d
Sergi Rene <sergi@dymension.xyz>
2024-09-16 16:18:19 +0200

    error log blocksync cid improved

--- store/store.go
+++ store/store.go
@@ -13,6 +13,7 @@ import (
 
 	"github.com/dymensionxyz/dymint/types"
 	pb "github.com/dymensionxyz/dymint/types/pb/dymint"
+	"github.com/dymensionxyz/gerr-cosmos/gerrc"
 )
 
 var (
@@ -288,7 +289,7 @@ func (s *DefaultStore) SaveBlockCid(height uint64, cid cid.Cid, batch KVBatch) (
 func (s *DefaultStore) LoadBlockCid(height uint64) (cid.Cid, error) {
 	cidBytes, err := s.db.Get(getCidKey(height))
 	if err != nil {
-		return cid.Undef, fmt.Errorf("load cid for height %v: %w", height, err)
+		return cid.Undef, fmt.Errorf("load cid for height %v: %w", height, gerrc.ErrNotFound)
 	}
 	parsedCid, err := cid.Parse(string(cidBytes))
 	if err != nil {
