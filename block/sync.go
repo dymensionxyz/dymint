e6cdb6cb021e4b62117e7fed3aa0c6767e5d2d3c
Sergi Rene <sergi@dymension.xyz>
2024-10-07 16:13:28 +0200

    fix intial seq update

--- block/sync.go
+++ block/sync.go
@@ -24,8 +24,6 @@ func (m *Manager) onNewStateUpdate(event pubsub.Message) {
 	m.LastSubmittedHeight.Store(eventData.EndHeight)
 	m.UpdateTargetHeight(eventData.EndHeight)
 
-	m.logger.Error("syncing")
-
 	// Update sequencers list from SL
 	err := m.UpdateSequencerSetFromSL()
 	if err != nil {
