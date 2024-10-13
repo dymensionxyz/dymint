98da008aa64e7a0648588af46fc1850ebe210123
Sergi Rene <sergi@dymension.xyz>
2024-10-13 19:09:33 +0200

    lint fix

--- block/validate.go
+++ block/validate.go
@@ -18,7 +18,7 @@ func (m *Manager) onNewStateUpdateFinalized(event pubsub.Message) {
 	}
 	m.State.SetLastFinalizedHeight(eventData.EndHeight)
 
-	//remove drs heights record for already finalized heights, since we do not need them for validation
+	// remove drs heights record for already finalized heights, since we do not need them for validation
 	m.State.ClearDRSVersionHeights(eventData.EndHeight)
 }
 
