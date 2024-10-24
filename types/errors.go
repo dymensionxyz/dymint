794bd8fe645e776e9366a371e18c2a3156de6fb1
Sergi Rene <sergi@dymension.xyz>
2024-10-24 17:06:35 +0200

    new frauds

--- types/errors.go
+++ types/errors.go
@@ -470,3 +470,75 @@ func getJsonFromBlock(block *Block) ([]byte, error) {
 	}
 	return jsonBlock, nil
 }
+
+type ErrStateUpdateBlobNotAvailableFraud struct {
+	StateIndex uint64
+	DA         string
+	DAHeight   uint64
+	Commitment string
+}
+
+func NewErrStateUpdateBlobNotAvailableFraud(stateIndex uint64, da string, daHeight uint64, commitment string) error {
+	return &ErrStateUpdateBlobNotAvailableFraud{
+		StateIndex: stateIndex,
+		DA:         da,
+		DAHeight:   daHeight,
+		Commitment: commitment,
+	}
+}
+
+func (e ErrStateUpdateBlobNotAvailableFraud) Error() string {
+	return fmt.Sprintf("blob not available in DA. StateIndex: %d DA: %s DA Height: %d Commitment: %s", e.StateIndex, e.DA, e.DAHeight, e.Commitment)
+}
+
+func (e ErrStateUpdateBlobNotAvailableFraud) Unwrap() error {
+	return gerrc.ErrFault
+}
+
+type ErrStateUpdateBlobCorruptedFraud struct {
+	StateIndex uint64
+	DA         string
+	DAHeight   uint64
+	Commitment string
+}
+
+func NewErrStateUpdateBlobCorruptedFraud(stateIndex uint64, da string, daHeight uint64, commitment string) error {
+	return &ErrStateUpdateBlobCorruptedFraud{
+		StateIndex: stateIndex,
+		DA:         da,
+		DAHeight:   daHeight,
+		Commitment: commitment,
+	}
+}
+
+func (e ErrStateUpdateBlobCorruptedFraud) Error() string {
+	return fmt.Sprintf("blob not parsable in DA. StateIndex: %d DA: %s DA Height: %d Commitment: %s", e.StateIndex, e.DA, e.DAHeight, e.Commitment)
+}
+
+func (e ErrStateUpdateBlobCorruptedFraud) Unwrap() error {
+	return gerrc.ErrFault
+}
+
+type ErrStateUpdateDRSVersionFraud struct {
+	StateIndex   uint64
+	Height       uint64
+	BlockVersion string
+	SLVersion    string
+}
+
+func NewErrStateUpdateDRSVersionFraud(stateIndex uint64, height uint64, blockVersion string, slVersion string) error {
+	return &ErrStateUpdateDRSVersionFraud{
+		StateIndex:   stateIndex,
+		Height:       height,
+		BlockVersion: blockVersion,
+		SLVersion:    slVersion,
+	}
+}
+
+func (e ErrStateUpdateDRSVersionFraud) Error() string {
+	return fmt.Sprintf("drs version not matching. StateIndex: %d Height: %d Block DRS: %s SL DRS: %s", e.StateIndex, e.Height, e.BlockVersion, e.SLVersion)
+}
+
+func (e ErrStateUpdateDRSVersionFraud) Unwrap() error {
+	return gerrc.ErrFault
+}
