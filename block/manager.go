2a6881d5fa67679f9fccba0d3e12cd17018c6847
Sergi Rene <sergi@dymension.xyz>
2024-11-21 22:34:12 +0100

    lint fix

--- block/manager.go
+++ block/manager.go
@@ -423,8 +423,10 @@ func (m *Manager) freezeNode(err error) {
 		return
 	}
 	uevent.MustPublish(m.Ctx, m.Pubsub, &events.DataHealthStatus{Error: err}, events.HealthStatusList)
-	m.P2PClient.Close()
-	m.DAClient.Stop()
+	err = m.DAClient.Stop()
+	if err != nil {
+		m.logger.Error("stop data availability layer client", "error", err)
+	}
 	m.Cancel()
 }
 
