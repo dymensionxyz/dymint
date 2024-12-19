ac0bda75eda17d5a8b3ff989b6c0dd12596ee637
danwt <30197399+danwt@users.noreply.github.com>
2024-12-19 18:16:51 +0000

    remove bogus test

--- types/dym_header_test.go
+++ types/dym_header_test.go
@@ -1,17 +1,3 @@
 package types
 
-import (
-	"bytes"
-	"testing"
-
-	"github.com/stretchr/testify/require"
-)
-
-func TestEmptyNotEqualDefault(t *testing.T) {
-	cons, _ := consMessagesHash(nil)
-	extra := ExtraSignedData{
-		ConsensusMessagesHash: cons,
-	}
-	h := extra.hash()
-	require.False(t, bytes.Equal(h, defaultEvidenceHash))
-}
+// TODO:
