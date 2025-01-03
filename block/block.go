1b88227dd4794b221e68a52ce58b553b2555bd0f
Sergi Rene <sergi@dymension.xyz>
2025-01-03 16:49:50 +0100

    undo

--- block/block.go
+++ block/block.go
@@ -101,11 +101,9 @@ func (m *Manager) applyBlock(block *types.Block, commit *types.Commit, blockMeta
 			return fmt.Errorf("save block source: %w", err)
 		}
 
-		if responses.EndBlock.RollappParamUpdates != nil {
-			_, err = m.Store.SaveDRSVersion(block.Header.Height, responses.EndBlock.RollappParamUpdates.DrsVersion, nil)
-			if err != nil {
-				return fmt.Errorf("add drs version: %w", err)
-			}
+		_, err = m.Store.SaveDRSVersion(block.Header.Height, responses.EndBlock.RollappParamUpdates.DrsVersion, nil)
+		if err != nil {
+			return fmt.Errorf("add drs version: %w", err)
 		}
 		// Commit block to app
 		appHash, retainHeight, err = m.Executor.Commit(m.State, block, responses)
