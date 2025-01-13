fa345de39e5984e1ab1defb6dad19f863a44a867
danwt <30197399+danwt@users.noreply.github.com>
2025-01-13 14:25:25 +0000

    fix and fmt

--- da/stub/stub.go
+++ da/stub/stub.go
@@ -9,11 +9,9 @@ import (
 
 var _ da.DataAvailabilityLayerClient = &Layer{}
 
-type Layer struct {
-}
+type Layer struct{}
 
 func (l Layer) Init(config []byte, pubsubServer *pubsub.Server, kvStore store.KV, logger types.Logger, options ...da.Option) error {
-	//TODO implement me
 	panic("implement me")
 }
 
@@ -22,41 +20,33 @@ func (l Layer) DAPath() string {
 }
 
 func (l Layer) Start() error {
-	//TODO implement me
 	panic("implement me")
 }
 
 func (l Layer) Stop() error {
-	//TODO implement me
 	panic("implement me")
 }
 
 func (l Layer) SubmitBatch(batch *types.Batch) da.ResultSubmitBatch {
-	//TODO implement me
 	panic("implement me")
 }
 
 func (l Layer) GetClientType() da.Client {
-	//TODO implement me
 	panic("implement me")
 }
 
 func (l Layer) CheckBatchAvailability(daMetaData *da.DASubmitMetaData) da.ResultCheckBatch {
-	//TODO implement me
 	panic("implement me")
 }
 
 func (l Layer) WaitForSyncing() {
-	//TODO implement me
 	panic("implement me")
 }
 
 func (l Layer) GetMaxBlobSizeBytes() uint32 {
-	//TODO implement me
 	panic("implement me")
 }
 
 func (l Layer) GetSignerBalance() (da.Balance, error) {
-	//TODO implement me
 	panic("implement me")
 }
