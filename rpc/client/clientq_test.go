8bfec78473474dfb8aa4e1112da1023f4cae56a9
danwt <30197399+danwt@users.noreply.github.com>
2025-02-21 16:43:04 +0000

    cp

--- rpc/client/clientq_test.go
+++ rpc/client/clientq_test.go
@@ -8,6 +8,10 @@ func TestFoo(t *testing.T) {
 	t.Log(a, b, err)
 	a, b, err = filterMinMax(900, 1000, 950, 970)
 	t.Log(a, b, err)
+	a, b, err = filterMinMax(900, 1000, 1000, 1000)
+	t.Log(a, b, err)
+	a, b, err = filterMinMax(900, 1000, 999, 999)
+	t.Log(a, b, err)
 	a, b, err = filterMinMax(1001, 1000, 0, 0)
 	t.Log(a, b, err)
 
