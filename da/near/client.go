1e8be266932c6796186e3cf8245a666bec352cdb
Sergi Rene <sergi@dymension.xyz>
2025-03-17 22:11:36 +0100

    put/get

--- da/near/client.go
+++ da/near/client.go
@@ -1,30 +1,19 @@
-package avail
+package near
 
 import (
-	"context"
-	"encoding/hex"
-	"errors"
-
-	"github.com/availproject/avail-go-sdk/metadata"
-	prim "github.com/availproject/avail-go-sdk/primitives"
-	"github.com/availproject/avail-go-sdk/sdk"
-	availgo "github.com/availproject/avail-go-sdk/sdk"
-	"github.com/dymensionxyz/dymint/da"
-	"github.com/ethereum/go-ethereum/common"
 	near "github.com/near/rollup-data-availability/gopkg/da-rpc"
-	"github.com/rs/zerolog/log"
 )
 
 type NearClient interface {
-	SubmitData(data []byte) (string, error)
-	GetBlock(blockHash string) (sdk.Block, error)
+	SubmitData(data []byte) ([]byte, error)
+	GetBlob(frameRef []byte) ([]byte, error)
 	GetAccountAddress() string
-	GetBlobsBySigner(blockHash string, accountAddress string) ([]availgo.DataSubmission, error)
 }
 
 type NearConfig struct {
 	Enable    bool
 	Account   string
+	Network   string
 	Contract  string
 	Key       string
 	Namespace uint32
@@ -39,7 +28,7 @@ var _ NearClient = &Client{}
 // NewClient returns a DA avail client
 func NewClient(config NearConfig) (NearClient, error) {
 
-	near, err := near.NewConfig(config.Account, config.Contract, config.Key, config.Namespace)
+	near, err := near.NewConfig(config.Account, config.Contract, config.Key, config.Network, config.Namespace)
 	if err != nil {
 		return nil, err
 	}
@@ -51,90 +40,26 @@ func NewClient(config NearConfig) (NearClient, error) {
 }
 
 // SubmitData sends blob data to Avail DA
-func (c Client) SubmitData(data []byte) (string, error) {
+func (c Client) SubmitData(data []byte) ([]byte, error) {
 	frameRefBytes, err := c.near.ForceSubmit(data)
-	if err != nil {
-		return "", err
-	}
-	frameRef := near.FrameRef{}
-	err = frameRef.UnmarshalBinary(frameRefBytes)
 	if err != nil {
 		return nil, err
 	}
 
-	return hex.EncodeToString(frameRefBytes), nil
+	return frameRefBytes, nil
 }
 
-// GetBlock retrieves a block from Avail chain by block hash
-func (c Client) GetBlock(blockHash string) (sdk.Block, error) {
-	hash, err := prim.NewH256FromHexString(blockHash)
+// GetBlock retrieves a block from Near chain by block hash
+func (c Client) GetBlob(frameRef []byte) ([]byte, error) {
+	blob, err := c.near.Get(frameRef, 0)
 	if err != nil {
-		return sdk.Block{}, errors.Join(da.ErrProofNotMatching, err)
+		return nil, err
 	}
 
-	block, err := sdk.NewBlock(c.sdk.Client, hash)
-	if err != nil {
-		return sdk.Block{}, errors.Join(da.ErrRetrieval, err)
-	}
-	return block, nil
-}
-
-// IsSyncing returns true if remote rpc node is still syncing
-func (c Client) IsSyncing() (bool, error) {
-	value, err := c.sdk.Client.Rpc.System.Health()
-	if err != nil {
-		return false, err
-	}
-	return value.IsSyncing, nil
+	return blob, nil
 }
 
 // GetAccountAddress returns configured account address
 func (c Client) GetAccountAddress() string {
-	return c.account.SS58Address(42)
-}
-
-// GetBlobsBySigner returns posted blobs filtered by block and sequencer account
-func (c Client) GetBlobsBySigner(blockHash string, accountAddress string) ([]availgo.DataSubmission, error) {
-	syncing, err := c.IsSyncing()
-	if syncing || err != nil {
-		return nil, errors.Join(err, da.ErrDANotAvailable)
-	}
-
-	block, err := c.GetBlock(blockHash)
-	if err != nil {
-		return nil, err
-	}
-	accountId, err := metadata.NewAccountIdFromAddress(accountAddress)
-	if err != nil {
-		return nil, err
-	}
-
-	// Block Blobs filtered by Signer
-	return block.DataSubmissionBySigner(accountId), nil
-}
-
-func (c Client) keysetHash() ([32]byte, []byte, error) {
-	svc := ServiceDetails{
-		service:     (DataAvailabilityServiceWriter)(s),
-		pubKey:      s.pub,
-		signersMask: 1,
-		metricName:  "near",
-	}
-
-	services := make([]ServiceDetails, 1)
-	services[0] = svc
-	return KeysetHashFromServices(services, 1)
-}
-
-func (s *NearService) GetByHash(ctx context.Context, hash common.Hash) ([]byte, error) {
-	log.Info("Getting message", "hash", hash)
-	// Hack to bypass commitment
-	bytesPadded := make([]byte, 64)
-	copy(bytesPadded[0:32], hash.Bytes())
-	bytes, err := s.near.Get(bytesPadded, 0)
-	if err != nil {
-		return nil, err
-	}
-	return bytes, nil
-
+	return ""
 }
--- da/near/near.go
+++ da/near/near.go
@@ -1,22 +1,29 @@
-package stub
+package near
 
 import (
 	"context"
+	"encoding/hex"
+	"encoding/json"
 	"fmt"
-	"strconv"
 	"strings"
 	"time"
-	near "github.com/near/rollup-data-availability/gopkg/da-rpc"
 
 	"cosmossdk.io/math"
+	"github.com/avast/retry-go/v4"
 	"github.com/dymensionxyz/dymint/da"
 	"github.com/dymensionxyz/dymint/da/stub"
 	"github.com/dymensionxyz/dymint/store"
 	"github.com/dymensionxyz/dymint/types"
+	"github.com/dymensionxyz/dymint/types/metrics"
 	"github.com/tendermint/tendermint/libs/pubsub"
 )
 
-const maxBlobSize = 500000
+const (
+	maxBlobSize               = 500000
+	defaultTxInclusionTimeout = 100 * time.Second
+	defaultBatchRetryDelay    = 10 * time.Second
+	defaultBatchRetryAttempts = 10
+)
 
 type Config struct {
 	Seed        string `json:"seed"`
@@ -27,9 +34,7 @@ type Config struct {
 // ToPath converts a SubmitMetaData to a path.
 func (d *SubmitMetaData) ToPath() string {
 	path := []string{
-		strconv.FormatUint(uint64(d.AppId), 10),
-		d.AccountAddress,
-		d.BlockHash,
+		hex.EncodeToString(d.FrameRef),
 	}
 	for i, part := range path {
 		path[i] = strings.Trim(part, da.PathSeparator)
@@ -40,20 +45,15 @@ func (d *SubmitMetaData) ToPath() string {
 // FromPath parses a path to a SubmitMetaData.
 func (d *SubmitMetaData) FromPath(path string) (*SubmitMetaData, error) {
 	pathParts := strings.FieldsFunc(path, func(r rune) bool { return r == rune(da.PathSeparator[0]) })
-	if len(pathParts) != 3 {
+	if len(pathParts) != 1 {
 		return nil, fmt.Errorf("invalid DA path")
 	}
-	appId, err := strconv.ParseUint(pathParts[0], 10, 32)
+
+	ref, err := hex.DecodeString(pathParts[0])
 	if err != nil {
 		return nil, err
 	}
-
-	submitData := &SubmitMetaData{
-		AppId: uint32(appId), //nolint:gosec
-	}
-
-	submitData.AccountAddress = pathParts[1]
-	submitData.BlockHash = pathParts[2]
+	submitData := &SubmitMetaData{FrameRef: ref}
 	return submitData, nil
 }
 
@@ -64,8 +64,9 @@ var (
 
 type DataAvailabilityLayerClient struct {
 	stub.Layer
+	client             NearClient
 	pubsubServer       *pubsub.Server
-	config             Config
+	config             NearConfig
 	logger             types.Logger
 	ctx                context.Context
 	cancel             context.CancelFunc
@@ -76,12 +77,15 @@ type DataAvailabilityLayerClient struct {
 
 // SubmitMetaData contains meta data about a batch on the Data Availability Layer.
 type SubmitMetaData struct {
-	// Avail App Id
-	AppId uint32
-	// Hash that identifies the blob
-	AccountAddress string
 	// Height is the height of the block in the da layer
-	BlockHash string
+	FrameRef []byte
+}
+
+// WithRPCClient sets rpc client.
+func WithRPCClient(client NearClient) da.Option {
+	return func(daLayerClient da.DataAvailabilityLayerClient) {
+		daLayerClient.(*DataAvailabilityLayerClient).client = client
+	}
 }
 
 // WithTxInclusionTimeout is an option which sets the timeout for waiting for transaction inclusion.
@@ -107,12 +111,47 @@ func WithBatchRetryAttempts(attempts uint) da.Option {
 
 // Init initializes DataAvailabilityLayerClient instance.
 func (c *DataAvailabilityLayerClient) Init(config []byte, pubsubServer *pubsub.Server, _ store.KV, logger types.Logger, options ...da.Option) error {
+	c.logger = logger
+
+	if len(config) > 0 {
+		err := json.Unmarshal(config, &c.config)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Set defaults
+	c.pubsubServer = pubsubServer
+
+	// TODO: Make configurable
+	c.txInclusionTimeout = defaultTxInclusionTimeout
+	c.batchRetryDelay = defaultBatchRetryDelay
+	c.batchRetryAttempts = defaultBatchRetryAttempts
+
+	// Apply options
+	for _, apply := range options {
+		apply(c)
+	}
+
+	metrics.RollappConsecutiveFailedDASubmission.Set(0)
 	return nil
 }
 
 // Start starts DataAvailabilityLayerClient instance.
 func (c *DataAvailabilityLayerClient) Start() error {
 	c.logger.Info("Starting Avail Data Availability Layer Client.")
+
+	// other client has already been set
+	if c.client != nil {
+		c.logger.Info("Avail client already set.")
+		return nil
+	}
+
+	client, err := NewClient(c.config)
+	if err != nil {
+		return fmt.Errorf("error while establishing connection to DA layer: %w", err)
+	}
+	c.client = client
 	return nil
 }
 
@@ -142,7 +181,69 @@ func (c *DataAvailabilityLayerClient) SubmitBatch(batch *types.Batch) da.ResultS
 	}
 
 	c.logger.Debug("Submitting to da batch with size", "size", len(blob))
-	return da.ResultSubmitBatch{} //c.submitBatchLoop(blob)
+	return c.submitBatchLoop(blob)
+}
+
+// submitBatchLoop tries submitting the batch. In case we get a configuration error we would like to stop trying,
+// otherwise, for network error we keep trying indefinitely.
+func (c *DataAvailabilityLayerClient) submitBatchLoop(dataBlob []byte) da.ResultSubmitBatch {
+	for {
+		select {
+		case <-c.ctx.Done():
+			return da.ResultSubmitBatch{}
+		default:
+			var frameBytes []byte
+			err := retry.Do(
+				func() error {
+					var err error
+					frameBytes, err = c.client.SubmitData(dataBlob)
+					if err != nil {
+						metrics.RollappConsecutiveFailedDASubmission.Inc()
+						c.logger.Error("broadcasting batch", "error", err)
+						return err
+					}
+					return nil
+				},
+				retry.Context(c.ctx),
+				retry.LastErrorOnly(true),
+				retry.Delay(c.batchRetryDelay),
+				retry.DelayType(retry.FixedDelay),
+				retry.Attempts(c.batchRetryAttempts),
+			)
+			if err != nil {
+				err = fmt.Errorf("broadcast data blob: %w", err)
+
+				if !retry.IsRecoverable(err) {
+					return da.ResultSubmitBatch{
+						BaseResult: da.BaseResult{
+							Code:    da.StatusError,
+							Message: err.Error(),
+							Error:   err,
+						},
+					}
+				}
+
+				c.logger.Error(err.Error())
+				continue
+			}
+			metrics.RollappConsecutiveFailedDASubmission.Set(0)
+			submitMetadata := &SubmitMetaData{
+				FrameRef: frameBytes,
+			}
+
+			c.logger.Debug("Successfully submitted batch.")
+			return da.ResultSubmitBatch{
+				BaseResult: da.BaseResult{
+					Code:    da.StatusSuccess,
+					Message: "success",
+				},
+				SubmitMetaData: &da.DASubmitMetaData{
+					DAPath: submitMetadata.ToPath(),
+					Client: da.Avail,
+				},
+			}
+		}
+	}
 }
 
 // RetrieveBatches retrieves batch from DataAvailabilityLayerClient instance.
@@ -152,7 +253,64 @@ func (c *DataAvailabilityLayerClient) RetrieveBatches(daPath string) da.ResultRe
 	if err != nil {
 		return da.ResultRetrieveBatch{BaseResult: da.BaseResult{Code: da.StatusError, Message: "wrong da path", Error: err}}
 	}
-	return da.ResultRetrieveBatch{}
+
+	blob, err := c.client.GetBlob(daMetaData.FrameRef)
+	if err != nil {
+		return da.ResultRetrieveBatch{
+			BaseResult: da.BaseResult{
+				Code:    da.StatusError,
+				Message: "Blob not found",
+				Error:   err,
+			},
+		}
+	}
+
+	var batches []*types.Batch
+	batch := &types.Batch{}
+	err = batch.UnmarshalBinary(blob)
+	if err != nil {
+		c.logger.Error("Unmarshaling blob", "error", err)
+		return da.ResultRetrieveBatch{
+			BaseResult: da.BaseResult{
+				Code:    da.StatusError,
+				Message: "Err Unmarshal",
+				Error:   err,
+			},
+		}
+	}
+
+	// Add the batch to the list
+	batches = append(batches, batch)
+	// Remove the bytes we just decoded.
+	size := batch.ToProto().Size()
+	if len(blob) < size {
+		c.logger.Error("Batch size does not match blob data length", "blob data length", len(blob), "batch size", size)
+		return da.ResultRetrieveBatch{
+			BaseResult: da.BaseResult{
+				Code:    da.StatusError,
+				Message: "Wrong size",
+				Error:   fmt.Errorf("wrong size"),
+			},
+		}
+	}
+
+	// if no batches, return error
+	if len(batches) == 0 {
+		return da.ResultRetrieveBatch{
+			BaseResult: da.BaseResult{
+				Code:    da.StatusError,
+				Message: "Blob not found",
+				Error:   da.ErrBlobNotFound,
+			},
+		}
+	}
+
+	return da.ResultRetrieveBatch{
+		BaseResult: da.BaseResult{
+			Code: da.StatusSuccess,
+		},
+		Batches: batches,
+	}
 }
 
 // CheckBatchAvailability checks batch availability in DataAvailabilityLayerClient instance.
