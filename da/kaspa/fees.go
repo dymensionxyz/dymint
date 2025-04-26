02cef49dc58ce1f4e95f9537ce361fe894a9e260
Sergi Rene <sergi@dymension.xyz>
2025-04-26 22:50:20 +0200

    skip max fee

--- da/kaspa/fees.go
+++ da/kaspa/fees.go
@@ -174,10 +174,10 @@ func (s *Client) maybeSplitAndMergeTransaction(transaction *serialization.Partia
 			return nil, err
 		}
 
-		err = s.checkTransactionFeeRate(splitTransactions[i], maxFee)
+		/*err = s.checkTransactionFeeRate(splitTransactions[i], maxFee)
 		if err != nil {
 			return nil, err
-		}
+		}*/
 	}
 
 	if len(splitTransactions) > 1 {
