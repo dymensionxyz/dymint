846a1cef47f152993c24a38b789203fba840b4ac
nuxtreact <nuxtreact@outlook.com>
2026-02-15 19:58:14 +0800

    refactor: use slices.Contains to simplify code (#1511)
    Signed-off-by: nuxtreact <nuxtreact@outlook.com>

--- indexers/txindex/kv/utils.go
+++ indexers/txindex/kv/utils.go
@@ -1,17 +1,14 @@
 package kv
 
+import "slices"
+
 import "github.com/google/orderedcode"
 
 const TxEventHeightKey = "txevent.height"
 
 // IntInSlice returns true if a is found in the list.
 func intInSlice(a int, list []int) bool {
-	for _, b := range list {
-		if b == a {
-			return true
-		}
-	}
-	return false
+	return slices.Contains(list, a)
 }
 
 func eventHeightKey(height int64) ([]byte, error) {
